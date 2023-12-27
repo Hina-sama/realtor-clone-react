1e82aac0a4769e96cf74a17406df72fb8df82179
Hina-sama <htdquynh2709@gmail.com>
2023-12-27 21:12:57 +0700

    Update code and files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-    <h1>Sun Flower</h1>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/forgot-password" element={<ForgotPassword/>} />
+        <Route path="/offers" element={<Offers/>} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/signin" element={<SignIn/>} />
+        <Route path="/signup" element={<SignUp/>} />
+      </Routes>
+    </Router>
     </>
   );
 }
